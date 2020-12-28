02f0f865b1e51aa6304f2889093f095575cf78aa
Abraham <abrdirect@gmail.com>
2020-12-27 23:16:24 -0800

    Change from many to one function, svg img added

--- js/app.js
+++ js/app.js
@@ -1,37 +1,4 @@
 
-
-function playClap() {
-    document.querySelector('#clap').play();
-}
-
-function playHiHat() {
-    document.querySelector('#hihat').play();
-}
-
-function playKick() {
-    document.querySelector('#kick').play();
-}
-
-function playOpenHat() {
-    document.querySelector('#openhat').play();
-}
-
-function playBoom() {
-    document.querySelector('#boom').play();
-}
-
-function playRide() {
-    document.querySelector('#ride').play();
-}
-
-function playSnare() {
-    document.querySelector('#snare').play();
-}
-
-function playTom() {
-    document.querySelector('#tom').play();
-}
-
-function playTink() {
-    document.querySelector('#tink').play();
+function playSound (id) {
+    document.querySelector(`#${id}`).play();
 }
