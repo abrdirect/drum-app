58820977acc29f21d4fbbc7dc7f8ee102e98a00a
Abraham <abrdirect@gmail.com>
2020-12-28 20:12:20 -0800

    screenshot added, replace keypress for keydown

--- js/app.js
+++ js/app.js
@@ -5,17 +5,13 @@ function playSound(id) {
     audio.play();
 }
 
-document.addEventListener('keypress', function(key){
-    const audio = document.querySelector(`[data-key="${key.code}"`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-});
-
 document.addEventListener('keydown', function(key) {
+    const audio = document.querySelector(`[data-key="${key.code}"`);
     const keyboard = document.querySelector(`[data-keyboard="${key.code}"`);
     if (!keyboard) return;
     keyboard.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
 });
 
 document.addEventListener('keyup', function(key) {
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # Simple Drum App with Vanilla JavaScript
+
+
+Screenshot:
+
+![Simple Drum App](https://github.com/abrdirect/drum-app/blob/master/img/screenshot.png)
