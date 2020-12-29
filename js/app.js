345a4e974c04fbd06a5926e12609bbe1ce1b7524
Abraham <abrdirect@gmail.com>
2020-12-28 19:43:20 -0800

    press pads with keyboard

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 
 function playSound(id) {
-    document.querySelector(`#${id}`).play();
+    const audio = document.querySelector(`#${id}`);
+    audio.currentTime = 0;
+    audio.play();
 }
 
-document.addEventListener('keypress', function(){
-    document.querySelector(`#clap`).play();
-  });
+document.addEventListener('keypress', function(key){
+    const audio = document.querySelector(`[data-key="${key.code}"`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+});
+
+document.addEventListener('keydown', function(key) {
+    const keyboard = document.querySelector(`[data-keyboard="${key.code}"`);
+    if (!keyboard) return;
+    keyboard.classList.add('playing');
+});
+
+document.addEventListener('keyup', function(key) {
+    const keyboard = document.querySelector(`[data-keyboard="${key.code}"`);
+    if (!keyboard) return;
+    keyboard.classList.remove('playing');
+});
